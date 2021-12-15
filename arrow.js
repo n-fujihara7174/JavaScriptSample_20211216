7ccfb1476fc417aadb59fa13ac58b6d15dd8d7bb
n-fujihara7174 <n.fujihara7174@gmail.com>
2021-12-16 07:56:44 +0900

    arrow関数サンプル

---
+++ arrow.js
@@ -0,0 +1,8 @@
+let getTriangle = (base, height) => {
+  return base * 2 / height;
+}
+
+console.log(getTriangle(5,1));
+getTriangle = (base,height) => base * 2 / height;
+
+console.log(getTriangle(10,5));
